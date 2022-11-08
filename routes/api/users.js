e5dd0260694d999200a4c13cdc51cff9a7501b80
msi-ism <msism720@gmail.com>
2022-11-07 21:54:06 -0500

    infrastructure complete

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,9 +3,10 @@ const express = require('express')
 
 const router = express.Router()
 const usersCtrl = require('../../controllers/api/users')
+const ensureLoggedIn = require('../../config/ensureLoggedIn')
 
 // ^ // GET /api/users/check-token
-router.get('/check-token', usersCtrl.checkToken)
+router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken)
 
 router.post('/login', usersCtrl.login)
 
